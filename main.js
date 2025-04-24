6c2b111999b44f95c9508a54f5d22d586c76ead5
wilarthur <will.arthur@hotmail.com>
2025-04-24 10:15:40 +0100

    Fixed menu color bug

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function toggleMenu() {
   const hamburger = document.getElementById("hamburger");
   element.classList.toggle("open");
   hamburger.classList.toggle("is-active");
+  nav.classList.toggle("open-menu-color");
 }
 
 const nav = document.getElementById("header");
