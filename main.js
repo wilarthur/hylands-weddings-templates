e1e3ffd2854748de809204a9c4e8473ed0da7398
Wil Arthur <will.arthur@hotmail.com>
2025-07-01 10:18:36 +0100

    Gallery lightbox added

--- main.js
+++ main.js
@@ -53,3 +53,25 @@ function observerCallback(entries) {
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 
 elementsToLoadIn.forEach((el) => observer.observe(el));
+
+/* Gallery lightbox */
+
+$(document).ready(function () {
+  $(".gallery-images").each(function () {
+    // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: "a", // the selector for gallery item
+      type: "image",
+      // gallery: {
+      //   enabled: true,
+      //   navigateByImgClick: true,
+      //   arrowMarkup:
+      //     '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+      //   tPrev: "Previous (Left arrow key)", // title for left button
+      //   tNext: "Next (Right arrow key)", // title for right button
+      //   tCounter: '<span class="mfp-counter">%curr% of %total%</span>', // markup of counter
+      //   titleSrc: "title",
+      // },
+    });
+  });
+});
