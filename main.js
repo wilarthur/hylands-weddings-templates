2fc395b7e93b562848d41fd8f3c827eeac70d6d2
wilarthur <will.arthur@hotmail.com>
2025-10-24 10:06:34 +0100

    Lowered intersection threshold to 10%

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ const elementsToLoadIn = document.querySelectorAll(".fadein");
 const observerOptions = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.3,
+  threshold: 0.1,
 };
 
 function observerCallback(entries) {
